e20b3d15df4895cf679031ff537d5f312ef9be3c
jonybang <Jonybange@gmail.com>
2019-06-30 18:32:49 +0200

    use geesome-libs

--- listen-pubsub.js
+++ listen-pubsub.js
@@ -1,22 +1,12 @@
 const util = require('util');
 const IPFS = require('ipfs');
-const ipns = require('ipns');
 const DaemonFactory = require('ipfsd-ctl');
-const base64url = require('base64url');
-const { fromB58String } = require('multihashes');
-const peerId = require('peer-id');
-
-const namespace = '/record/';
-
-const _createPeerIdFromPubKey = util.promisify(peerId.createFromPubKey);
+const JsIpfsServiceNode = require("@galtproject/geesome-libs/src/JsIpfsServiceNode");
+const ipfsHelper = require("@galtproject/geesome-libs/src/ipfsHelper");
 
 module.exports = async (listenToNodeAddress, ipnsToSubscribe) => {
   console.log('listenToNodeAddress', listenToNodeAddress);
   console.log('ipnsToSubscribe', ipnsToSubscribe);
-  
-  const multihash = fromB58String(ipnsToSubscribe);
-  const idKeys = ipns.getIdKeys(multihash);
-  const topic = `${namespace}${base64url.encode(idKeys.routingKey.toBuffer())}`;
   
   const df = DaemonFactory.create({ type: 'proc' });
   const _dfSpawn = util.promisify(df.spawn).bind(df);
@@ -39,24 +29,20 @@ module.exports = async (listenToNodeAddress, ipnsToSubscribe) => {
     preload: { enabled: false }
   });
 
-  const _swarmConnect = util.promisify(node.api.swarm.connect).bind(node.api.swarm);
-  await _swarmConnect(listenToNodeAddress);
+  const ipfsService = new JsIpfsServiceNode(node.api);
   
-  const _pubSubSubscribe = util.promisify(node.api.pubsub.subscribe).bind(node.api.pubsub);
-
-  await _pubSubSubscribe(topic, async (event) => {
-    event.key = await _createPeerIdFromPubKey(event.key);
-    event.data = ipns.unmarshal(event.data);
-    event.data.peerId = await _createPeerIdFromPubKey(event.data.pubKey);
-
-    event.data.value = event.data.value.toString('utf8');
+  await ipfsService.swarmConnect(listenToNodeAddress);
+  
+  await ipfsService.subscribeToIpnsUpdates(ipnsToSubscribe, async (event) => {
     console.log('event emitted!', event);
   });
   
-  await _pubSubSubscribe('fruit', (event) => {
+  await ipfsService.pubSubSubscribe('fruit', (event) => {
     event.data = event.data.toString('utf8');
     console.log('fruit event emitted!', event);
   });
+  
+  const topic = ipfsHelper.getIpnsUpdatesTopic(ipnsToSubscribe);
 
   console.log('subscribed to ' + topic);
   
--- publish-pubsub.js
+++ publish-pubsub.js
@@ -1,28 +1,12 @@
 const util = require('util');
-const assert = require('assert');
-const _ = require('lodash');
-const noop = () => {
-};
-
-const FloodSub = require('libp2p-floodsub');
-const {signMessage} = require('libp2p-pubsub/src/message/sign');
-const peerId = require('peer-id');
-const crypto = require('libp2p-crypto');
-const errcode = require('err-code');
-const asyncMap = require('async/map');
-const waterfall = require('async/waterfall');
-const nextTick = require('async/nextTick');
 const retry = require('async/retry');
-const {message, utils} = require('libp2p-pubsub');
-const ensureArray = utils.ensureArray
 
 const JsIpfsServiceNode = require("@galtproject/geesome-libs/src/JsIpfsServiceNode");
+const ipfsHelper = require("@galtproject/geesome-libs/src/ipfsHelper");
 
 const IPFS = require('ipfs');
 
 (async () => {
-  const _keyLookup = util.promisify(keyLookup);
-  const _createPeerIdFromPrivKey = util.promisify(peerId.createFromPrivKey);
   const _waitForPeerToSubscribe = util.promisify(waitForPeerToSubscribe);
 
   const node = new IPFS({
@@ -35,7 +19,6 @@ const IPFS = require('ipfs');
   
   const _nodeId = util.promisify(node.id);
 
-  // console.log('node', node);
   try {
     await new Promise((resolve, reject) => {
       node.on('ready', (err) => err ? reject(err) : resolve());
@@ -48,40 +31,24 @@ const IPFS = require('ipfs');
   }
 
   const ipfsService = new JsIpfsServiceNode(node);
-  //
+
   const selfIpns =  await ipfsService.getAccountIdByName('self');
   console.log('self ipns', selfIpns);
-  //
+
   const pubSubTestsName = 'pubsub-tests';
   await ipfsService.createAccountIfNotExists(pubSubTestsName);
 
   const pubSubTestsIpns = await ipfsService.getAccountIdByName(pubSubTestsName);
   console.log('pubsub-tests ipns', pubSubTestsIpns);
 
-  // console.log('node', node);
-  // console.log('node._repo.keys', node._repo.keys);
-
-  const pubSubTestsPrivateKey = await _keyLookup(node, 'pubsub-tests');
+  const pubSubTestsPrivateKey = await ipfsService.keyLookup('pubsub-tests');
   console.log('pubSubTestsPrivateKey', pubSubTestsPrivateKey);
-  const pubSubTestsPeerId = await _createPeerIdFromPrivKey(pubSubTestsPrivateKey.bytes);
+  const pubSubTestsPeerId = await ipfsHelper.createPeerIdFromPrivKey(pubSubTestsPrivateKey.bytes);
   console.log('pubSubTestsPeerId', pubSubTestsPeerId);
 
-  // console.log('node', node);
-  // console.log('node._floodSub', node._floodSub);
-
-  // const fsub = new FloodSub(node.libp2p);
-  // fsub.peerId = pubSubTestsPeerId;
-  const fsub = node.libp2p._floodSub;
-  // //
-  improveFloodSub(fsub);
-  improvePubSub(fsub);
-
-  // console.log('node.libp2p.peerInfo.id', node.libp2p.peerInfo.id);
-  //
-  fsub.on('fruit', (data) => {
+  ipfsService.subscribeToEvent('fruit', (data) => {
     console.log('on fruit', data)
   });
-  fsub.subscribe('fruit');
 
   const nodeId = await _nodeId();
   const listenData = await require('./listen-pubsub')(nodeId.addresses[0], selfIpns);
@@ -99,95 +66,10 @@ const IPFS = require('ipfs');
   });
 
 
-  fsub.publishByPeerId(pubSubTestsPeerId, 'fruit', new Buffer('banana'), () => {});
-  // node.pubsub.publish('fruit', new Buffer('banana2'), (err) => {
-  //   console.log('err', err);
-  // });
+  ipfsService.publishEventByPeerId(pubSubTestsPeerId, 'fruit', 'banana', () => {});
 })();
 
 //peerId.createFromPrivKey(Buffer.from(opts.privateKey, 'base64'), cb)
-
-function improveFloodSub(fsub) {
-  //https://github.com/libp2p/js-libp2p-floodsub/blob/4feadeb9ef0cc35892a9c499c740e759b3d73ec8/src/index.js#L167
-  fsub.publishByPeerId = (function (peerId, topics, messages, callback) {
-    assert(this.started, 'FloodSub is not started')
-    callback = callback || noop
-
-    this.log('publish', topics, messages)
-
-    topics = ensureArray(topics)
-    messages = ensureArray(messages)
-
-    const from = this.libp2p.peerInfo.id.toB58String();
-
-    const buildMessage = (msg, cb) => {
-      const seqno = utils.randomSeqno()
-      this.seenCache.put(utils.msgId(from, seqno))
-
-      const message = {
-        from: from,
-        data: msg,
-        seqno: seqno,
-        topicIDs: topics
-      }
-
-      // Emit to self if I'm interested
-      this._emitMessages(topics, [message])
-
-      this._buildMessageByPeerId(peerId, message, cb)
-    }
-
-    asyncMap(messages, buildMessage, (err, msgObjects) => {
-      if (err) return callback(err)
-
-      // send to all the other peers
-      this._forwardMessages(topics, msgObjects)
-
-      callback(null)
-    })
-  }).bind(fsub);
-
-  fsub.publish = function (topics, messages, callback) {
-    this.publishByPeerId(this.peerId, topics, messages, callback);
-  };
-}
-
-
-function improvePubSub(fsub) {
-  // https://github.com/libp2p/js-libp2p-pubsub/blob/f1e188929d779e7af91e1fd039b2c3b95cdf05df/src/index.js#L246
-  fsub._buildMessageByPeerId = (function (peerId, message, callback) {
-    const msg = utils.normalizeOutRpcMessage(message)
-    if (peerId) {
-      signMessage(peerId, msg, callback)
-    } else {
-      nextTick(callback, null, msg)
-    }
-  }).bind(fsub);
-  
-  fsub._buildMessage = (function (message, callback) {
-    this._buildMessageByPeerId(this.peerId, message, callback)
-  }).bind(fsub);
-}
-
-
-function keyLookup(ipfsNode, kname, callback) {
-  if (kname === 'self') {
-    return callback(null, ipfsNode._peerInfo.id.privKey)
-  }
-  const pass = ipfsNode._options.pass
-
-  waterfall([
-    (cb) => ipfsNode._keychain.exportKey(kname, pass, cb),
-    (pem, cb) => crypto.keys.import(pem, pass, cb)
-  ], (err, privateKey) => {
-    if (err) {
-      log.error(err)
-      return callback(errcode(err, 'ERR_CANNOT_GET_KEY'))
-    }
-
-    return callback(null, privateKey)
-  })
-}
 
 function waitForPeerToSubscribe (node, topic, callback) {
   retry({
